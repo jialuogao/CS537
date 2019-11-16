23b9b91a5b37575d900301f9e9c55bde0577b361
Jialuo Gao <jialuogao980513@gmail.com>
2019-11-16 16:22:11 -0600

    init all project xv6 and other files

---
+++ checkformat.sh
@@ -0,0 +1,4 @@
+cpplint.py --extensions=c,h part1/cache1D.c
+cpplint.py --extensions=c,h part1/cache2Drows.c
+cpplint.py --extensions=c,h part1/cache2Dcols.c
+cpplint.py --extensions=c,h part2/csim.c
